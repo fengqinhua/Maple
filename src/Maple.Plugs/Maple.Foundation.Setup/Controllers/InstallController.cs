99f3421e29d2cd4dc89d22dc72cf52a5b7ef01c6
Fengqinhua <fengqinhua2016@163.com>
2018-04-16 15:45:04 +0800

    添加Install相关MVC文件

---
+++ src/Maple.Plugs/Maple.Foundation.Setup/Controllers/InstallController.cs
@@ -0,0 +1,33 @@
+﻿using Maple.Core.Configuration;
+using Maple.Core.Data;
+using Maple.Foundation.Setup.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maple.Foundation.Setup.Controllers
+{
+    public partial class InstallController : Controller
+    {
+        private readonly MapleConfig _config;
+
+        public InstallController(MapleConfig config)
+        {
+            this._config = config;
+        }
+
+        public virtual IActionResult Index()
+        {
+            if (DataSettingsHelper.DatabaseIsInstalled())
+                return RedirectToRoute("HomePage");
+
+            var model = new InstallModel
+            {
+                AdminName = "admin@maple.com"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/Maple.Plugs/Maple.Foundation.Setup/Models/InstallModel.cs
@@ -0,0 +1,27 @@
+﻿using Maple.Web.Framework.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maple.Foundation.Setup.Models
+{
+    public class InstallModel : BaseMapleModel
+    {
+        /// <summary>
+        /// 超级管理员名称
+        /// </summary>
+        public string AdminName { get; set; }
+        /// <summary>
+        /// 超级管理密码
+        /// </summary>
+        public string AdminPassword { get; set; }
+        /// <summary>
+        /// 超级管理员密码
+        /// </summary>
+        public string ConfirmPassword { get; set; }
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        public string DatabaseConnectionString { get; set; }
+    }
+}
