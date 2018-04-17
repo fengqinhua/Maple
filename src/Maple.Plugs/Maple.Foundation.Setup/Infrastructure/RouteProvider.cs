d0f89beb5f12bb9fb4fbad062aacf286b468bb92
Fengqinhua <fengqinhua2016@163.com>
2018-04-17 14:20:17 +0800

    为插件添加自定义IRouteProvider

---
+++ src/Maple.Plugs/Maple.Foundation.Setup/Infrastructure/RouteProvider.cs
@@ -0,0 +1,23 @@
+﻿using Maple.Web.Framework.Mvc.Routing;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maple.Foundation.Setup.Infrastructure
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute(SetupFoundationDefaluts.InstallRoute,
+                "Plugins/Install", new { controller = "Install", action = "Index" });
+        }
+
+        public int Priority
+        {
+            get { return 0; }
+        }
+    }
+}
---
+++ src/Maple.Plugs/Maple.Foundation.Setup/SetupFoundationDefaluts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maple.Foundation.Setup
+{
+    public static class SetupFoundationDefaluts
+    {
+        public static string InstallRoute => "Maple.Foundation.Setup.Install";
+    }
+}
