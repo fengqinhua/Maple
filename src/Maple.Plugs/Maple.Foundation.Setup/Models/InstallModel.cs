6f156c26098cfe1a53efe02f80bec89fd2febed2
Fengqinhua <fengqinhua2016@163.com>
2018-05-09 18:08:34 +0800

    属性字段中添加多语言标签

--- src/Maple.Plugs/Maple.Foundation.Setup/Models/InstallModel.cs
+++ src/Maple.Plugs/Maple.Foundation.Setup/Models/InstallModel.cs
@@ -1,4 +1,5 @@
-﻿using Maple.Web.Framework.Mvc.Models;
+﻿using Maple.Web.Framework.Mvc.ModelBinding;
+using Maple.Web.Framework.Mvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,18 +11,23 @@ namespace Maple.Foundation.Setup.Models
         /// <summary>
         /// 超级管理员名称
         /// </summary>
+        [MapleResourceDisplayName("Maple.Foundation.Setup.Models.InstallModel.AdminName")]
         public string AdminName { get; set; }
         /// <summary>
         /// 超级管理密码
         /// </summary>
+        [MapleResourceDisplayName("Maple.Foundation.Setup.Models.InstallModel.AdminPassword")]
         public string AdminPassword { get; set; }
         /// <summary>
         /// 超级管理员密码
         /// </summary>
+        [MapleResourceDisplayName("Maple.Foundation.Setup.Models.InstallModel.ConfirmPassword")]
+        [NoTrim]
         public string ConfirmPassword { get; set; }
         /// <summary>
         /// 数据库连接字符串
         /// </summary>
+        [MapleResourceDisplayName("Maple.Foundation.Setup.Models.InstallModel.DatabaseConnectionString")]
         public string DatabaseConnectionString { get; set; }
     }
 }
