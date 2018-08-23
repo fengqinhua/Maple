7dcddb7657e0567c6c027e229944409e8497419a
Fengqinhua <fengqinhua2016@163.com>
2018-08-23 14:55:53 +0800

    添加AssembluInfo

---
+++ src/Maple.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Reflection;
+
+//如想深度定制，可参考
+//https://www.cnblogs.com/zyl910/p/dotnet_standard_cs0579_duplicate_attribute.html
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Maple.Core.Tests")]
